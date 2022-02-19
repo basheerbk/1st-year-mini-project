3b396cde04240264d96e1cc88708c8cce3007411
Muhammad Basheer <49371247+basheerbk@users.noreply.github.com>
2022-02-19 10:23:26 +0530

    Update Final_Code.ino

--- code/Final_Code.ino
+++ code/Final_Code.ino
@@ -1,1 +1,35 @@
-ok
+int lmPin = A0; 
+int ledPin = 10;
+int relayPin = 8;
+
+void setup()
+{
+pinMode(lmPin, INPUT);
+pinMode(relayPin, OUTPUT);
+pinMode(ledPin,  OUTPUT);  
+Serial.begin(9600);
+}
+void loop()
+{
+int val;
+val=analogRead(0);
+
+if (val >= 63)
+{
+  delay(500);
+  digitalWrite(ledPin, HIGH);
+  delay(3000); 
+  digitalWrite(relayPin, LOW);
+}
+else
+{  
+  digitalWrite(relayPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+}
+
+Serial.print("Value =   ");
+Serial.println(val);// output and display value of val
+
+delay(1000);// wait for 1 second
+}
